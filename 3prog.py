6049166e0d933a2f967bb4529be3dfbb5975f5b5
Sreejanaareddy <66524968+Sreejanaareddy@users.noreply.github.com>
2020-06-07 10:23:29 +0530

    3prgm.py
    this programs are about list tuple and dictionaries

---
+++ 3prog.py
@@ -0,0 +1,18 @@
+#accessing elements from tuple using index
+my_tuple = ('p','e','r','m','i','t')
+print(my_tuple[0])
+print(my_tuple[5]) 
+
+#assigning elements to different lists
+list1 = ['physics', 'chemistry', 1997, 2000];
+list2 = [1, 2, 3, 4, 5 ];
+list3 = ["a", "b", "c", "d"]
+ 
+#deleting different dictionary elements
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
